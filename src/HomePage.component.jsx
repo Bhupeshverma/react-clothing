d248ba044e171358f6c47da22f4100177ad73bd1
BhupeshVerma <bhupesh.v94@gmail.com>
2020-05-12 09:35:41 +0530

    created react app with create react app and added file for hompage

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+const Homepage = () => (
+    <div className="homepage">
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        HAT
+                    </div>
+                    <div className='subtitle'>
+                        SHOP NOW    
+                    </div>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        JACKETS
+                    </div>
+                    <div className='subtitle'>
+                        SHOP NOW    
+                    </div>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        WOMENS
+                    </div>
+                    <div className='subtitle'>
+                        SHOP NOW    
+                    </div>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        MENS
+                    </div>
+                    <div className='subtitle'>
+                        SHOP NOW    
+                    </div>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>
+                        HAT
+                    </div>
+                    <div className='subtitle'>
+                        SHOP NOW    
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+)
+
+export default Homepage
